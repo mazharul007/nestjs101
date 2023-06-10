91fabeeff3d9be451e16fc98ca6b5805b79485ad
mazharul <mazharul@redlimesolutions.com>
2023-06-10 17:41:12 +0600

    created module, controller , service and defined a task model

---
+++ src/tasks/tasks.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+
+@Controller('tasks')
+export class TasksController {
+
+    constructor(private tasksService : TasksService) {}
+
+    @Get()
+    fetchAllTasks(){
+        return this.tasksService.getAllTasks();
+    }
+
+}
---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class TasksService {
+
+    
+    private tasks = [];
+
+    getAllTasks(){
+        return this.tasks;
+        
+    }
+
+}
